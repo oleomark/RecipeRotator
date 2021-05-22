a75879d52a0b47140ee387d7470fee1d3e37b663
Aromee Kim <aromeekim@aromees-mbp.home>
2021-05-22 00:06:28 -0700

    creates recipe model

--- reciperotator/main_app/models.py
+++ reciperotator/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model): 
+    name = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+    instructions = models.TextField(max_length=500)
+    servingsize = models.IntegerField()
+    author = models.CharField(max_length=100)
+
+    def __str__(self): 
+        return self.name
