a0beb51e666105d49215bfc56a26144dc41a58ee
Mark Oleo <oleo.mark@gmail.com>
2021-05-22 16:51:41 -0700

    Create/Update/Delete

--- reciperotator/main_app/models.py
+++ reciperotator/main_app/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
+
+CUISINES = (('NA', 'North American'), ('SA','South American'), ('LA', 'Latin American'), ('AS', 'Asian'), ('EU', 'European'), ('MD', 'Mediterranean'), ('AF', 'African'))
 # Create your models here.
 class Recipe(models.Model): 
     name = models.CharField(max_length=100)
-    cuisine = models.CharField(max_length=100)
+    cuisine = models.CharField(
+        max_length=3,
+        choices=CUISINES,
+        default=CUISINES[0][0]
+    )
     instructions = models.TextField(max_length=500)
     servingsize = models.IntegerField()
     calories = models.IntegerField()
@@ -11,3 +18,7 @@ class Recipe(models.Model):
 
     def __str__(self): 
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={"recipe_id": self.id})
+    
--- reciperotator/main_app/views.py
+++ reciperotator/main_app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Recipe
 
 # Create your views here.
+class RecipeCreate(CreateView):
+    model = Recipe
+    fields = '__all__'
+    success_url = '/recipes/'
+
+class RecipeUpdate(UpdateView):
+  model = Recipe
+  fields = '__all__'
+
+class RecipeDelete(DeleteView):
+  model = Recipe
+  success_url = '/recipes/'
+
+
+
 def home(request): 
     return render(request, 'home.html')
 
