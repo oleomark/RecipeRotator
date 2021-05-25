afe79843f09d45ddca183befb5c4cd2d8c68e659
Aromee Kim <aromeekim@aromees-mbp.home>
2021-05-24 23:49:40 -0700

    adds functionality to upload photos

--- reciperotator/main_app/admin.py
+++ reciperotator/main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Recipe, RecipeLog, Ingredient
+from .models import Recipe, RecipeLog, Ingredient, Photo
 
 # Register your modelss here.
 admin.site.register(Recipe)
 admin.site.register(RecipeLog)
 admin.site.register(Ingredient)
+admin.site.register(Photo)
